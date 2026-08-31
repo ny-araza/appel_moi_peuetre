6811bfc263478ba1dd619bba2ce495fee4d69036
Ny Aina Tsilavina Razafimahatratra <ny-araza@c1r3s12.42antananarivo.mg>
2026-08-31 15:04:10 +0300

    add constraint for generate function name

--- src/utils.py
+++ src/utils.py
@@ -3,17 +3,28 @@ from llm_sdk import Small_LLM_Model
 from .parse import parse, read_file
 from typing import Any
 
-def get_response(model: Small_LLM_Model, prompt: str, max_token_input: int):
+def get_response(model: Small_LLM_Model, prompt: str, functions_ids: list[list[int]]):
     input_ids = model.encode(prompt).tolist()[0]
     i = 0
-    print(max_token_input)
-    while prompt and i < max_token_input:
-        logits = model.get_logits_from_input_ids(input_ids)
-        max_token = max(logits)
-        token = logits.index(max_token)
-        input_ids.append(token)
-        print(model.decode([token]), end="")
-        i += 1
+
+    logits = model.get_logits_from_input_ids(input_ids)
+    res = ""
+    for i in range(len(functions_ids)):
+        for j in range(len(functions_ids[i])):
+            k = 0
+            while functions_ids[i][j] == functions_ids[k][j]:
+                k += 1                 
+            if k == len(functions_ids):
+                res.append(functions_ids[i][j])
+            else:
+                pass
+    # while prompt and i < get_max_func_len(functions_ids):
+
+    #     max_token = max(logits)
+    #     token = logits.index(max_token)
+    #     input_ids.append(token)
+    #     print(model.decode([token]), end="")
+    #     i += 1
 
 
 def encode_prompt(prompt: str, model: Small_LLM_Model) -> None:
@@ -23,7 +34,6 @@ def encode_prompt(prompt: str, model: Small_LLM_Model) -> None:
 
 def get_max_func_len(functions_name: list[list[int]]) -> int:
     length_tab = []
-    print(functions_name)
     for l in functions_name:
         length_tab.append(len(l))
     return max(length_tab)
@@ -46,6 +56,6 @@ def encode_functions_name(model: Small_LLM_Model, config: dict[str, Any]) -> Non
              "calling. Choose one from the functions name with its " \
              f"description listed below to answer the prompt:\n{var}\n" \
              f"The prompt is: {prompt_list[2]["prompt"]}\n"\
-             "The function name is : "    
+             "The function name is : "
 
-    get_response(model, prompt, get_max_func_len(all_function_name))
+    get_response(model, prompt, all_function_name)
