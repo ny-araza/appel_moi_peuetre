b2272db1e9ec00a4e57f25c55a98793331cb64ba
Ny Aina Tsilavina Razafimahatratra <ny-araza@c1r2s7.42antananarivo.mg>
2026-09-07 14:14:30 +0300

    update

--- src/__main__.py
+++ src/__main__.py
@@ -3,11 +3,13 @@ from llm_sdk import Small_LLM_Model
 import sys
 from .parse import parse
 import os
+from .output import generate_json_file
 
 if __name__ == "__main__":
     try:
         model = Small_LLM_Model()
         config = parse(sys.argv[:1])
-        print(get_function_name(model, config))
+        result = get_function_name(model, config)
+
     except Exception as e:
         print(f"An error occured: {e}")
--- src/output.py
+++ src/output.py
@@ -1,1 +1,5 @@
 from typing import Any
+import json
+
+def generate_json_file(filename: str, res_json: list[dict[Any, Any]]) -> None:
+    print(json.dump(res_json))
