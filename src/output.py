5ac2646f4207ed5bb9371c8c9865ab4fabbe4666
Ny Aina Tsilavina Razafimahatratra <ny-araza@c1r2s7.42antananarivo.mg>
2026-09-08 11:47:53 +0300

    add export in jsonfile

--- src/output.py
+++ src/output.py
@@ -14,6 +14,5 @@ def validate_output(res_json: list[dict[Any, Any]]) -> bool:
     pass
 
 def generate_json_file(filename: str, res_json: list[dict[Any, Any]]) -> None:
-    # adapter = TypeAdapter(list[Output])
-    print(json.dumps(res_json, indent=2))
-    # adapter.validate_json(str(res_json))
+    with open(filename, "w") as fd:
+        json.dump(res_json, fd,indent=2)
